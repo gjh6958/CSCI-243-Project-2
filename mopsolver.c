46f7e2d1843a325eba72308f479a712e8f2c6fef
Grayson James Hassell <gjh6958@cs.rit.edu>
2018-10-26 13:52:27 -0400

    Created first files in project, the main development file and the main header file.  Stubbed out main function, will stub out the rest of the functions and structures tonight.

---
+++ mopsolver.c
@@ -0,0 +1,13 @@
+/*
+ * Author - Grayson Hassell
+ * CSCI-243 Project 2
+ * "Escape From GNU York"
+ * Maze solving program
+ */
+
+#include "mopsolver.h"
+
+
+int main( int argc, char* argv[] ){
+
+}
---
+++ mopsolver.h
@@ -0,0 +1,16 @@
+/*
+ * Header file for the mopsolver file
+ * defines functions and describes their functionality
+ * includes other libraries
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef MOPSOLVER
+#define MOPSOLVER
+
+int main( int argc, char *argv[] );
+
+
+#endif
