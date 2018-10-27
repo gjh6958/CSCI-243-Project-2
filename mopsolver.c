0c328fae16a28043579997d6113fb582850a9c61
Grayson James Hassell <gjh6958@cs.rit.edu>
2018-10-27 14:58:25 -0400

    Finished stubbing out planned functions, flushed out functionalify for option_h and option_d and fixed file handling for outfile.  Program can now read in a maze and print it in user friendly formatting.  Next I will implement the maze solving algorithm.

--- mopsolver.c
+++ mopsolver.c
@@ -6,8 +6,171 @@
  */
 
 #include "mopsolver.h"
+#include <string.h>
 
+void getdims( FILE *fp, int dims[] ){
+   int N = 0; // rows
+   int M = 0; // cols
+   char c;
+
+   while( c = fgetc( fp ) ){
+      if( c == '\n' )
+         N++;
+      else if( c == EOF )
+         break;
+      else
+         M++;
+   }
+
+   M = (M/N);
+
+   printf("Rows: %d, Cols: %d\n", N, M);
+
+   dims[0] = N;
+   dims[1] = M;
+
+   Rows = N;
+   Cols = M;
+}
+
+/// Tis fucntion reads the maze from a given file
+int read_maze( FILE *fp, int dims[], char maze[Rows][Cols] ){
+
+   char c;
+   int colcount = 0;
+   int rowcount = 0;
+
+   for( rowcount; rowcount < Rows; rowcount++ ){
+      for( colcount; colcount < Cols; colcount++ ){
+         c = fgetc( fp );
+         if( c == '\n' )
+            c = fgetc( fp );
+         if( c == EOF )
+            return 0;
+         maze[rowcount][colcount] = c;
+         printf("%c", maze[rowcount][colcount]);
+      }
+      colcount = 0;
+      printf("\n");
+   }
+}
+
+/// This function writes the maze to a given file
+int write_maze( FILE *fp ){
+
+}
+
+/// prints an optimal path
+void option_p( int i ){
+
+}
+
+/// Prints shortest solution steps
+void option_s( int i){
+
+}
+
+void pptopbot( FILE *out ){
+   fprintf(out, "|");
+   for( int i = 0; i < Cols * 2 + 1; i++)
+      fprintf(out, "-");
+   fprintf(out, "|\n");
+
+}
+
+/// Pretty prints the maze after reading
+void option_d( char maze[Rows][Cols], FILE *out ){
+   pptopbot( out );
+   for( int rowcount = 0; rowcount < Rows; rowcount++ ){
+      fprintf(out, "| ");
+      for( int colcount = 0; colcount < Cols; colcount++ ){
+         if( maze[rowcount][colcount] == '0' )
+            fprintf(out, ". ");
+         else if( maze[rowcount][colcount] == '1' )
+            fprintf(out, "# ");
+      }
+      fprintf(out, "|\n");
+   }
+   pptopbot( out );
+}
+
+/// Prints helpful mesaage to stdout and exit
+void option_h( int i, FILE* outfile ){
+   fprintf(outfile, "USAGE:\n");
+   fprintf(outfile, "mopsolver [-hdsp] [-i INFILE] [-o OUTFILE]\n\n");
+   fprintf(outfile, "Options:\n");
+   fprintf(outfile, "     -h      Print this helpful message to stdout and exit.\n");
+   fprintf(outfile, "     -d      Pretty print (display) the maze after reading.  (Default: off)\n");
+   fprintf(outfile, "     -s      Print shortest solution steps.        	     (Default: off)\n");
+   fprintf(outfile, "     -p      Print an optimal path.                	     (Default: off)\n");
+   fprintf(outfile, "     -i INFILE       Read maze from INFILE.        	     (Default: stdin)\n");
+   fprintf(outfile, "     -o OUTFILE      Write all output to OUTFILE.  	     (Default: stdout)\n");
+
+}
 
 int main( int argc, char* argv[] ){
+
+   int p = 0;
+   int s = 0;
+   int d = 0;
+   int h = 0;
+   char *infile = NULL;
+   char *outfile = NULL;
+   FILE *fip = stdin;
+   FILE *fop = stdout;
+
+   for( int i = 1; i < argc; i++ ){
+      for( unsigned int j = 0; j < strlen( argv[i] ); j++ ){
+         if( argv[i][0] == '-' ){
+            if( argv[i][j] == 'p' )
+               p = 1;
+            if( argv[i][j] == 's' )
+               s = 1;
+            if( argv[i][j] == 'd' )
+               d = 1;
+            if( argv[i][j] == 'h' )
+               h = 1;
+         }
+         else if( strcmp( argv[i-1], "-i" ) ){
+            outfile = argv[i];
+         }
+         else if( strcmp( argv[i-1], "-o" ) ){
+            infile = argv[i];
+         }
+      }
+   }
+
+
+   printf("\nFlags set, p: %d s: %d d: %d h: %d", p, s, d, h);
+   printf("\nInfile: %s", infile);
+   printf("\nOutfile: %s\n", outfile);
+   if(outfile != NULL)
+      fop = fopen( outfile, "w" );
+   if(infile != NULL)
+      fip = fopen( infile, "r" );
+
+   if( h ){
+      option_h( h, fop );
+      return 0;
+   }
+
+   int dims[2];
+   if( infile != NULL){
+      int dims[2];
+      getdims( fip, dims );
+   }
+   char maze[Rows][Cols];
+   if( infile != NULL ){
+      fip = fopen( infile, "r" );
+      read_maze( fip, dims, maze );
+   }
+   if( d )
+      option_d( maze, fop );
+   if( s )
+      option_s( s );
+   if( p )
+      option_p( p );
+
+   return 0;
 
 }
--- mopsolver.h
+++ mopsolver.h
@@ -10,6 +10,27 @@
 #ifndef MOPSOLVER
 #define MOPSOLVER
 
+int Rows = 0;
+int Cols = 0;
+
+/// Tis fucntion writes the maze that was parsed to a specific file
+int write_maze( FILE *fp );
+
+/// This function reades the maze from a given file
+int read_maze( FILE *fp, int dims[], char maze[Rows][Cols] );
+
+/// prints an optimal path
+void option_p( int i );
+
+/// Prints shortest solution steps
+void option_s( int i);
+
+/// Pretty prints the maze after reading
+void option_d( char maze[Rows][Cols], FILE *out );
+
+/// Prints helpful mesaage to stdout and exit
+void option_h( int i, FILE* out );
+
 int main( int argc, char *argv[] );
 
 
